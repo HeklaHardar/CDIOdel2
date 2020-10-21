6dbd9dbb9b32842b5773695d50d25ea994cb2a95
lau.thomsen.privat@gmail.com <lau.thomsen.privat@gmail.com>
2020-10-21 16:51:20 +0200

    Pointsystem which actually works :))

--- src/Game/Account.java
+++ src/Game/Account.java
@@ -2,4 +2,24 @@ package Game;
 
 public class Account {
 
+    private int points = 1500;
+
+
+    public Account(String name){
+    }
+
+    //Updates player score
+    public void updateScore(int sum) {
+
+        points += sum;
+
+
+    }
+
+    //returns new score
+    public int score() {
+        return points;
+    }
+
+
 }
--- src/Game/Board.java
+++ src/Game/Board.java
@@ -2,12 +2,19 @@ package Game;
 
 public class Board {
     public static void main(String[] args) {
-        int roll;
         Dices d1 = new Dices();
-        int sum = d1.sum();
-        Fields f1 = new Fields(sum);
-        int new_money = f1.getMoney();
+        Account ac1 = new Account("Player1");
 
+
+        for (int i = 0; i < 1000; i++){
+            d1.roll();
+            int sum = d1.sum();
+            Fields f1 = new Fields(sum);
+            int new_money = f1.getMoney();
+            ac1.updateScore(new_money);
+            int score = ac1.score();
+            System.out.println("New Score:" + score);
+        }
 
 
     }
