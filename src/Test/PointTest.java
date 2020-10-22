47ba4c19238fe6ca15786623d4ee7fd341168f8f
Mikkel <mikkelsa97@gmail.com>
2020-10-22 11:01:09 +0200

    Started PointTest

--- src/Test/PointTest.java
+++ src/Test/PointTest.java
@@ -1,4 +1,24 @@
 package Test;
 
+import Game.Dices;
+import Game.Die;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class PointTest {
+
+    @Test
+    public void getPointTest(){
+
+
+        for(int i = 1000; i > 0; --i){
+
+
+
+        }
+
+
+    }
+
+
 }
